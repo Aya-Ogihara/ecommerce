ad08d9f6d6df4177e69c5da59c58286e5ba369be
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2023-03-15 21:50:29 +1300

    ✨ Replace actual data from dummy data

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,20 +1,28 @@
 import Link from 'next/link';
 import React from 'react';
 
-const HeroBanner = () => {
+import { urlFor } from '../lib/client';
+
+const HeroBanner = ({ bannerInfo }) => {
+  console.log(bannerInfo);
   return (
     <div className='hero-banner-container'>
       <div>
-        <p className='beats-solo'>SMALL TEXT</p>
-        <h3>MID TEXT</h3>
-        <img src='' alt='headphones' className='hero-banner-image' />
+        <p className='beats-solo'>{bannerInfo.smallText}</p>
+        <h3>{bannerInfo.midText}</h3>
+        <h1>{bannerInfo.largeText1}</h1>
+        <img
+          src={urlFor(bannerInfo.image)}
+          alt='headphones'
+          className='hero-banner-image'
+        />
         <div>
-          <Link href='/product/ID'>
-            <button type='button'>BUTTON TEXT</button>
+          <Link href={`/product/${bannerInfo.product}`}>
+            <button type='button'>{bannerInfo.buttonText}</button>
           </Link>
           <div className='desc'>
             <h5>Description</h5>
-            <p>DESCRIPTION</p>
+            <p>{bannerInfo.desc}</p>
           </div>
         </div>
       </div>
